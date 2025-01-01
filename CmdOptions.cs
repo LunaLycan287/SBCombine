775ee9d49086174acd748001f6bdfe464b8a5af3
LunaLycan287 <1161223+LunaLycan287@users.noreply.github.com>
2025-01-01 01:54:46 +0100

    Updated Readme & Fixed some help output

--- CmdOptions.cs
+++ CmdOptions.cs
@@ -10,15 +10,15 @@ public class CmdOptions
    [Option('m', "main", HelpText = "Main file to put at the top. By default the first file found will be used.")]
    public string? Main { get; set; } 
    
-   [Option('i', "include", HelpText = "Include files from the list using glob pattern. If this is not set all *.cs files in the directory and any sub-directory will be used.")]
+   [Option('i', "include", HelpText = @"(Default: '**\*.cs') Include files from the list using glob pattern.")]
    public required IEnumerable<string> Include { get; set; }
    
-   [Option('e', "exclude", HelpText = @"Exclude files from the list using glob pattern. By default all files in '**\obj\*', '**\bin\*' and '**\Properties\AssemblyInfo.cs' are excluded. If you add excludes remember to add them back.")]
+   [Option('e', "exclude", HelpText = @"(Default: '**\obj\*', '**\bin\*', '**\Properties\AssemblyInfo.cs') Exclude files from the list using glob pattern.")]
    public  required IEnumerable<string> Exclude { get; set; }
    
-   [Option('o', "output", Default = "*-Merged.sbcs", HelpText = "Sets the output filename. '*' will be replaced with the main file name. By default it is '*-Merged.sbcs'.")]
+   [Option('o', "output", Default = "*-Merged.sbcs", HelpText = "(Default: '*-Merged.sbcs') Sets the output filename. '*' will be replaced with the main file name.")]
    public required string Output { get; set; }
    
-   [Option('u', "usings", Default = true, HelpText = "If common implicit usings should be added if dependent libraries are found. Default = true. (This is an ever growing list, please report new ones on github)")]
+   [Option('u', "usings", Default = true, HelpText = "(Default: true) If common implicit usings should be added if dependent libraries are found. (This is an ever growing list, please report new ones on github)")]
    public bool Usings { get; set; }
 }
